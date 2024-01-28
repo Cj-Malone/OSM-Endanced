f3017ddd06bd5a69adaf41dccb194832b87584dc
Cj Malone <CjMalone@mail.com>
2024-01-28 20:39:56 +0000

    Skip robots.txt
    Checking robots.txt doubles the requests to most domains, and we aren't being dicks

--- OSM_Enhanced/settings.py
+++ OSM_Enhanced/settings.py
@@ -19,7 +19,7 @@ COMMANDS_MODULE = "OSM_Enhanced.commands"
 # USER_AGENT = "OSM_Enhanced (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
